1affdd7b896aafa3580b3bc9149a1dbb6307d18c
arun <arun@arun-Vostro-3588>
2018-07-23 14:46:06 +0530

    added

--- ampereplus/settings.py
+++ ampereplus/settings.py
@@ -200,7 +200,7 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
